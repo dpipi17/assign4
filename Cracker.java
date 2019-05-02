4a12409e8109afcdbcda138b2e5515a7933e6e52
deme <dpipi17@freeuni.edu.ge>
2019-05-02 11:04:29 +0400

    Cracker finished

--- Cracker.java
+++ Cracker.java
@@ -1,9 +1,18 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.CountDownLatch;
+
 
 public class Cracker {
 	// Array of chars used to produce strings
 	public static final char[] CHARS = "abcdefghijklmnopqrstuvwxyz0123456789.,-!".toCharArray();	
+	
+	// private instance variables
+	private String passwordHash;
+	private int maxLength;
+	private CountDownLatch allFinished;
+	private String result;
+	private boolean found;
 	
 	/*
 	 Given a byte[] array, produces a hex String,
@@ -35,12 +44,145 @@ public class Cracker {
 		return result;
 	}
 	
-	// main method
-	public static void main(String[] args) {
-		if(args.length == 1) {
-			 
-		} else {
+	// stores information into instance variables and calls startWorkers
+	public void findPassword(String passwordHash, int maxLength, int numberOfWorkers) {
+		this.passwordHash = passwordHash;
+		this.maxLength = maxLength;
+		allFinished = new CountDownLatch(numberOfWorkers);
+		result = "No result Found";
+		found = false;
+		
+		startWorkers(numberOfWorkers);
+	}
+	
+	
+	// starts new workers 
+	private void startWorkers(int numberOfWorkers) {
+		// using this workers have roughly the same number of starting chars
+		int[] charsNum = new int[numberOfWorkers];
+		for(int i = 0; i < numberOfWorkers; i++) {
+			charsNum[i] = CHARS.length / numberOfWorkers;
+		}
+		
+		for(int i = 0; i < CHARS.length % numberOfWorkers; i++) {
+			charsNum[i]++;
+		}
 			
+		// starts new workers and finds start and end index for it
+		int currInd = 0;
+		for(int i = 0; i < numberOfWorkers; i++) {
+			int startInd = currInd;
+			int endInd = currInd + charsNum[i] - 1;
+			
+			Worker newWorker = new Worker(startInd, endInd);
+			newWorker.start();
+			currInd = endInd + 1;
+		}
+	}
+	
+	// returns hash of the given string 
+	public static String getHash(String str) {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+			messageDigest.update(str.getBytes());
+			return hexToString(messageDigest.digest());
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	// main thread waits on this CountDownLatch
+	public void waitOnAllFinished() {
+		try {
+			allFinished.await();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}		
+	}
+	
+	// after all prints result 
+	public void printResult() {
+		System.out.println(result);
+		System.out.println("all done");
+	}
+	
+
+
+	// inner Worker class
+	private class Worker extends Thread {
+		
+		// private instance variables of Worker
+		private int startInd;
+		private int endInd;
+		
+		// Worker Constructor
+		public Worker(int startInd, int endInd) {
+			this.startInd = startInd;
+			this.endInd = endInd;
+		}
+		
+		@Override
+	    public void run() {
+			doWork();
+	    }
+
+		// generates start index character using its start and end indexes
+		private void doWork() {
+			for(int ind = startInd; ind <= endInd; ind++) {
+				String startStr = "" + CHARS[ind];
+				recFinding(startStr);
+			}
+			// post for main thread
+			allFinished.countDown();
+		}
+		
+		// recursion string generator
+		// searches password using dfs algorithm
+		private void recFinding(String currStr) {
+			// if we already found the password 
+			// or this string is larger than max Size, worker has to stop working
+			if(currStr.length() > maxLength || found) return;
+			
+			if(match(currStr)) {
+				result = currStr;
+				found = true;
+				return;
+			}
+			
+			// adds new characters
+			for(int i = 0; i < CHARS.length; i++) {
+				recFinding(currStr + CHARS[i]);
+			}
+		}
+		
+		// determines if we found password
+		private boolean match(String str) {
+			return passwordHash.equals(getHash(str));
+		}
+	}
+	
+	
+	// main method
+	public static void main(String[] args) {		
+		
+		if(args.length == 1) {
+			// returns hash of the password
+			System.out.println(Cracker.getHash(args[0]));
+		} else if(args.length == 3){
+			// finds password using input 
+			String passwordHash = args[0];
+			int maxLength = Integer.parseInt(args[1]);
+			int numberOfWorkers = Integer.parseInt(args[2]);
+			
+			Cracker crack = new Cracker();
+			crack.findPassword(passwordHash, maxLength, numberOfWorkers);
+			crack.waitOnAllFinished();
+			crack.printResult();
+		} else {
+			System.out.println("Invalid Arguments");
 		}
 			
 	}
